2690a3a68cf07de5cb4025017730ddd0227d6b58
Daniel Bachler <daniel@danielbachler.de>
2016-06-06 17:09:56 +0200

    Implemented initial conversion smiles -> inchi

--- python-flask-server/controllers/default_controller.py
+++ python-flask-server/controllers/default_controller.py
@@ -1,3 +1,6 @@
+from rdkit import Chem
+from rdkit.Chem import inchi
 
 def smiles_to_inchi_get(smiles) -> str:
-    return 'hello from ChemIdConvert'
+    m = Chem.MolFromSmiles(smiles)
+    return inchi.MolToInchi(m)
