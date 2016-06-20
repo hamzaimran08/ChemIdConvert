df37dfe6df3b9d61b8843d3032d61fbb8eed0a6e
Daniel Bachler <daniel@danielbachler.de>
2016-06-20 16:57:32 +0200

    Implemented first passing version of simple test (provided the chemidconvert service is running and reachable at basePath)

--- tests/simple-conversions.ts
+++ tests/simple-conversions.ts
@@ -1,19 +1,17 @@
 import { Test, test } from 'tape'
 import { SMILESInChIApi, InlineResponse200} from './typescript-node-client/api'
 
+const basePath : string = "http://192.168.99.100:8080/v1"
+
 function verifyInchi(inchi : String, t : Test) : void {
   t.equal("InChI=1S/C2H6O/c1-2-3/h3H,2H2,1H3", inchi)
 }
 
 function testSmilesToInchi(t : Test) : void {
-    t.plan(2);
-
-    const api = new SMILESInChIApi()
+    const api = new SMILESInChIApi(basePath)
     api.smilesToInchiGet("CCO")
-      .then(value => verifyInchi(value.body.inchi, t)
-      , (error: any) => t.fail("Api call failed"))
-
-
+      .then(value => verifyInchi(value.body.inchi, t))
+      .error((error: any) => t.fail("Api call failed"))
 }
 
 test.test('smiles to inchi', testSmilesToInchi);
